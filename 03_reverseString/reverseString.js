196f15846cb0db0f6ae815264db08c89b1e81f1c
Your Name <94960890+Misterpeppa@users.noreply.github.com>
2025-05-04 22:24:11 +0800

    Completed js-exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let answer = "";
+    let letter = "";
+    let n = string.length;
+    for (let i = 1; i <= n; i++) {
+        letter = string.slice(-1);
+        string = string.slice(0, -1);
+        answer += letter;
+    }
+    
+    return answer;
 };
 
 // Do not edit below this line
